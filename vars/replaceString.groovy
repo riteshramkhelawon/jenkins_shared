bdc07f106d68f3609d0e93380d311fc42634cf57
riteshramkhelawon <53088880+riteshramkhelawon@users.noreply.github.com>
2020-10-22 13:38:48 +0100

    Update replaceString.groovy

--- vars/replaceString.groovy
+++ vars/replaceString.groovy
@@ -1,5 +1,6 @@
 def call(String filename){
+  sh "echo filename is ${filename}"
   def text = readFile file: "${filename}"
-    text = text.replaceAll("%BUILD_NUMBER%", "${BUILD_NUMBER}")
+  text = text.replaceAll("%BUILD_NUMBER%", "${BUILD_NUMBER}")
   writeFile file: "${filename}", text: text
 }
